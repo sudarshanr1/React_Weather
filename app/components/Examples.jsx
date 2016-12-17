bc798e18a0ab82d7c6d8353c07160b797b6d7640
sudarshanr1 <sudarshanr.ravindranath@gmail.com>
2016-12-17 15:40:17 -0800

    renamed text

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples Component!</h3>
+      <h3>Examples</h3>
       <p>This is just an example</p>
     </div>
   );
