7cd9b01776cf67cc2dfec53124de5a1e3ccf7b71
sudarshanr1 <sudarshanr.ravindranath@gmail.com>
2016-12-17 17:28:10 -0800

    style changes

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,18 @@
 var React = require('react');
+var {Link} = require('react-router');
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>This is just an example</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Try out some locations here</p>
+      <ol>
+        <li>
+          <Link to='/?location=Kolar Gold Fields'>Kolar Gold Fields, KA, India</Link>
+        </li>
+        <li>
+          <Link to='/?location=Kolar Gold Fields'>San Francisco, CA</Link>
+        </li>
+      </ol>
     </div>
   );
 }
