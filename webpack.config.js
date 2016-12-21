4a03584be2b88fb96804f3f0b7a5797a8d13dce5
sudarshanr1 <sudarshanr.ravindranath@gmail.com>
2016-12-21 15:14:44 -0800

    sass added

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
       Examples:'app/components/Examples.jsx',
       ErrorModal:'app/components/ErrorModal.jsx',
       openweathermap:'app/api/openWeatherMap.jsx',
-      applicationStyles:'app/styles/app.css'
+      applicationStyles:'app/styles/app.scss'
     },
     extensions:['','.js','.jsx']
   },
